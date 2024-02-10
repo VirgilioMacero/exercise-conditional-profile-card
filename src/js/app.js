cfd71fc0a7615b58c56f58b01715d813e3a822bc
Virgilio Macero <virgiliomacero@gmail.com>
2024-02-10 01:38:38 +0000

    My modifications

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,38 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name != "" ? variables.name : "Lucy"} ${
+    variables.lastname != "" ? variables.lastname : "Boilett"
+  }</h1>
+          <h2>${variables.role != "" ? variables.role : "Web Developer"}</h2>
+          <h3>${variables.city != "" ? variables.city : "Miami"}, ${
+    variables.country != "" ? variables.country : "USA"
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition != ""
+              ? variables.socialMediaPosition
+              : "position-right"
+          }">
+            <li><a href="${
+              variables.twitter != ""
+                ? variables.twitter
+                : "https://twitter.com/4geeksacademy"
+            }"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github != ""
+                ? variables.github
+                : "https://github.com/4geeksacademy"
+            }"><i class="fa fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin != ""
+                ? variables.linkedin
+                : "https://linkedin.com/school/4geeksacademy"
+            }"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram != ""
+                ? variables.instagram
+                : "https://instagram.com/4geeksacademy"
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -56,19 +80,20 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media.licdn.com/dms/image/C4D03AQGaTGde8Prnyw/profile-displayphoto-shrink_800_800/0/1596674591388?e=2147483647&v=beta&t=1K2vtd__gzyldUaUothC5Cs5L8B0i2SEi-p2MQ0w1T4",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
+    name: "",
+    lastname: "",
+    role: "",
+    country: "",
+    city: ""
   };
   render(window.variables); // render the card for the first time
 
